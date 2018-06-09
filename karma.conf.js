b5e63c6c61c6323f2761f2b71e0b30d33caab8fc
naker90 <AntonioJesusSG96@gmail.com>
2018-06-09 20:49:51 +0100

    Modify karma config for use webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,23 @@
+const webpackConfig = require('./webpack.config.js');
 module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-    files: [
-      'spec/**/*.spec.js'
-    ],
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['Chrome'],
-    singleRun: false,
-    concurrency: Infinity
+      basePath: '',
+      frameworks: ['jasmine'],
+      preprocessors: {
+          'spec/dependencies.js': ['webpack']
+      },
+      files: [
+        'spec/dependencies.js',
+        'spec/**/*.spec.js'
+      ],
+      reporters: ['progress'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: false,
+      browsers: ['Chrome'],
+      singleRun: false,
+      concurrency: Infinity,
+      webpack: webpackConfig
   })
 };
