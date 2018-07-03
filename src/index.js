5bf92533fd1d73a7e031abd0f2aedbdc7023e2bc
naker90 <AntonioJesusSG96@gmail.com>
2018-07-03 22:13:54 +0100

    #refactor rename

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const Navigator = require("./utils/navigator");
 const Router = require("./router/router");
 const Routes = require("./router/routes");
 
-setInterval(() => resolveRoutes(), 200);
+setInterval(() => initializeRouter(), 200);
 
-function resolveRoutes(){
+function initializeRouter(){
     let router = Router(Navigator(), Routes);
     let resolver = router.resolveRoute();
     resolver.resolve();
