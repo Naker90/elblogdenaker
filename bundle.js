b96cc66cdd14b78b7d23c54a26e37e29612af426
naker90 <AntonioJesusSG96@gmail.com>
2018-07-03 22:34:26 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -76,9 +76,10 @@
   !*** ./src/controllers/homeController.js ***!
   \*******************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("function test(){\n    NakerNoventa.Navigator.render(\"Estás en la home\");\n}\n\nmodule.exports = test;\n\n//# sourceURL=webpack:///./src/controllers/homeController.js?");
+"use strict";
+eval("\n\nvar Navigator = __webpack_require__(/*! ../utils/navigator */ \"./src/utils/navigator.js\");\n\nfunction test() {\n    var navigator = Navigator();\n    navigator.render(\"Estás en la home\");\n}\n\nmodule.exports = test;\n\n//# sourceURL=webpack:///./src/controllers/homeController.js?");
 
 /***/ }),
 
@@ -89,7 +90,8 @@ eval("function test(){\n    NakerNoventa.Navigator.render(\"Estás en la home\")
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("__webpack_require__(/*! ./styles/main-style.css */ \"./src/styles/main-style.css\");\n__webpack_require__(/*! ./styles/responsive-style.css */ \"./src/styles/responsive-style.css\");\n__webpack_require__(/*! ./utils/navigator */ \"./src/utils/navigator.js\");\n__webpack_require__(/*! ./router/routes */ \"./src/router/routes.js\");\n__webpack_require__(/*! ./router/router */ \"./src/router/router.js\");\n\n\nwindow.NakerNoventa = window.NakerNoventa || {};\nNakerNoventa.Router.initialize();\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("\n\n__webpack_require__(/*! ./styles/main-style.css */ \"./src/styles/main-style.css\");\n__webpack_require__(/*! ./styles/responsive-style.css */ \"./src/styles/responsive-style.css\");\n\nvar Navigator = __webpack_require__(/*! ./utils/navigator */ \"./src/utils/navigator.js\");\n\nvar Router = __webpack_require__(/*! ./router/router */ \"./src/router/router.js\");\nvar Routes = __webpack_require__(/*! ./router/routes */ \"./src/router/routes.js\");\n\nsetInterval(function () {\n    return initializeRouter();\n}, 200);\n\nfunction initializeRouter() {\n    var navigator = Navigator();\n    var router = Router(navigator, Routes);\n    var resolver = router.resolveRoute();\n    resolver.resolve();\n    navigator.setHashLocation(resolver.route);\n}\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -98,9 +100,10 @@ eval("__webpack_require__(/*! ./styles/main-style.css */ \"./src/styles/main-sty
   !*** ./src/router/router.js ***!
   \******************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("window.NakerNoventa = window.NakerNoventa || {};\n\n((NakerNoventa) => {\n\n    function Router(){\n\n        function initialize(){\n            setInterval(() => {\n                let route = NakerNoventa.Navigator.getLocationHash();\n                NakerNoventa.Routes.getRouteSolver(route, successCallback);\n            }, 500);\n\n            function successCallback(solver){\n                solver.solve();\n            }\n        }\n\n        return {\n            initialize: initialize\n        };\n    }\n\n    NakerNoventa.Router = Router();\n\n})(window.NakerNoventa);\n\n\n//# sourceURL=webpack:///./src/router/router.js?");
+"use strict";
+eval("\n\nfunction router(navigator, routes) {\n\n    function resolveRoute() {\n        var route = navigator.getLocationHash();\n        return getRouteSolver(route);\n    }\n\n    function getRouteSolver(route) {\n        return getRouteSolverIfThereIsAMatch() || getRouteSolverByDefault();\n\n        function getRouteSolverIfThereIsAMatch() {\n            return routes.filter(function (x) {\n                return x.route === route;\n            })[0];\n        }\n\n        function getRouteSolverByDefault() {\n            return routes.filter(function (x) {\n                return x.routeByDefault;\n            })[0];\n        }\n    }\n\n    return {\n        resolveRoute: resolveRoute\n    };\n}\n\nmodule.exports = router;\n\n//# sourceURL=webpack:///./src/router/router.js?");
 
 /***/ }),
 
@@ -111,7 +114,8 @@ eval("window.NakerNoventa = window.NakerNoventa || {};\n\n((NakerNoventa) => {\n
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("const HomeController = __webpack_require__(/*! ../controllers/homeController */ \"./src/controllers/homeController.js\");\n\nwindow.NakerNoventa = window.NakerNoventa || {};\n\n(function(NakerNoventa){\n\n    const routes = [\n        {\n            route: 'home',\n            solve: HomeController\n        }\n    ];\n\n    function Routes(){\n\n        function getRouteSolver(route, successCallback){\n            routes.map(function(x) {\n                if(x.route === route){\n                    successCallback(x);\n                }\n            })\n        }\n\n        return {\n            getRouteSolver: getRouteSolver\n        }\n    }\n\n    NakerNoventa.Routes = Routes();\n\n})(window.NakerNoventa);\n\n//# sourceURL=webpack:///./src/router/routes.js?");
+"use strict";
+eval("\n\nvar HomeController = __webpack_require__(/*! ../controllers/homeController */ \"./src/controllers/homeController.js\");\n\nmodule.exports = [{\n    route: 'home',\n    resolve: HomeController,\n    routeByDefault: true\n}];\n\n//# sourceURL=webpack:///./src/router/routes.js?");
 
 /***/ }),
 
@@ -142,9 +146,10 @@ eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./sr
   !*** ./src/utils/navigator.js ***!
   \********************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("window.NakerNoventa = window.NakerNoventa || {};\n\n((NakerNoventa) => {\n\n    function NavigatorContract(){\n        return {\n            getLocationHash: function(){},\n            render: function(){}\n        }\n    }\n\n    function Navigator(){\n        const HOME_ROUTE = 'home';\n        let contract = NavigatorContract();\n\n        function getLocationHash(){\n            return location.hash.slice(1) || HOME_ROUTE;\n        }\n\n        function render(content){\n            document.getElementById('app').innerHTML = content;\n        }\n\n        contract.getLocationHash = getLocationHash;\n        contract.render = render;\n        return contract;\n    }\n\n    NakerNoventa.Navigator = Navigator();\n\n})(window.NakerNoventa);\n\n//# sourceURL=webpack:///./src/utils/navigator.js?");
+"use strict";
+eval("\n\nfunction navigator() {\n\n    function getLocationHash() {\n        return location.hash.slice(1);\n    }\n\n    function render(content) {\n        document.getElementById('app').innerHTML = content;\n    }\n\n    function setHashLocation(location) {\n        window.location.hash = location;\n    }\n\n    return {\n        getLocationHash: getLocationHash,\n        render: render,\n        setHashLocation: setHashLocation\n    };\n}\n\nmodule.exports = navigator;\n\n//# sourceURL=webpack:///./src/utils/navigator.js?");
 
 /***/ })
 
