5f024f5366a253b8e10f1e9d35e36ac8ca05152d
naker90 <AntonioJesusSG96@gmail.com>
2018-07-07 23:43:23 +0100

    create factory for router

--- src/router/factory.js
+++ src/router/factory.js
@@ -1,3 +1,16 @@
-/**
- * Created by naker90 on 7/07/18.
- */
+const Navigator = require("../utils/navigator");
+const Router = require("./router");
+const Routes = require("./routes");
+
+function factory(){
+
+    function router(){
+        return Router(Navigator.navigator(), Routes)
+    }
+
+    return {
+        router: router
+    }
+}
+
+module.export = factory;
