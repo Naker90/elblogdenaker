3a3943f6f610b3b7b5f8b643b61c87c839aa16bb
naker90 <AntonioJesusSG96@gmail.com>
2018-10-06 23:09:12 +0100

    simplify factories

--- src/controllers/controllerFactory.js
+++ src/controllers/controllerFactory.js
@@ -1,15 +1,4 @@
 const navigator = require("../utils/navigator");
 const homeController = require("./homeController");
 
-function controllerFactory(){
-
-    function createHomeController(){
-        return homeController(navigator());
-    }
-
-    return {
-        createHomeController: createHomeController
-    }
-}
-
-module.exports = controllerFactory();
+export const createHomeController = () => homeController(navigator());
--- src/router/routerFactory.js
+++ src/router/routerFactory.js
@@ -1,15 +1,4 @@
 const router = require("./router");
 const routesResolver = require("./routesResolver");
 
-function routerFactory(){
-
-    function createRoutesResolver(){
-        return routesResolver(router());
-    }
-
-    return {
-        createRoutesResolver: createRoutesResolver
-    }
-}
-
-module.exports = routerFactory();
+export const createRoutesResolver = () => routesResolver(router());
