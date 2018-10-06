74b19e5d114f2622f0fd773fba53334ff82784b5
naker90 <AntonioJesusSG96@gmail.com>
2018-10-06 21:53:23 +0100

    delete old router

--- src/utils/navigator.js
+++ src/utils/navigator.js
@@ -1,8 +1,6 @@
 function navigatorContract(){
     return {
-        getLocationHash: function (){},
         render: function (){},
-        setHashLocation: function (){}
     }
 }
 
@@ -10,21 +8,11 @@ function navigator(){
 
     let contract = navigatorContract();
 
-    function getLocationHash(){
-        return location.hash.slice(1);
-    }
-
     function render(content){
         document.getElementById('app').innerHTML = content;
     }
 
-    function setHashLocation(location) {
-        window.location.hash = location
-    }
-
-    contract.getLocationHash = getLocationHash;
     contract.render = render;
-    contract.setHashLocation = setHashLocation;
     return contract;
 }
 
