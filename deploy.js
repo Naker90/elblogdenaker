e907320812c218c33f0881c528ca4c5329775a0b
naker90 <AntonioJesusSG96@gmail.com>
2018-10-12 17:08:29 +0100

    create deploy script

---
+++ deploy.js
@@ -0,0 +1,9 @@
+const ghPages = require("gh-pages");
+const branch = 'dist';
+const config = {
+    branch: 'master',
+    repo: 'https://github.com/Naker90/naker90.github.io.git'
+};
+const callback = () => console.log("Blog deployed in https://naker90.github.io");
+
+ghPages.publish(branch, config, callback);
