2af09ba0bf6a407df9aad88a473d451c6a715c7f
naker90 <AntonioJesusSG96@gmail.com>
2018-11-01 16:46:30 +0000

    delete create-react-class dependecy and use components

--- src/Home/Views/articlesList.jsx
+++ src/Home/Views/articlesList.jsx
@@ -1,35 +1,34 @@
 const React = require("react");
 const ReactDom = require("react-dom");
-const ReactCreateClass = require("create-react-class");
+
+class Article extends React.Component{
+    render(){
+        return (
+            <article>
+                <h2>{this.props.article.name}</h2>
+                <h3>{this.props.article.description}</h3>
+                <div>
+                    <p>{this.props.article.author}</p>
+                    <p>{this.props.article.date}</p>
+                </div>
+            </article>
+        )
+    }
+}
+
+class ArticlesList extends React.Component{
+    render(){
+        return (
+            <div>
+                {this.props.articles.map(function(article, key){
+                    return <Article key={key} article={article}/>
+                })}
+            </div>
+        )
+    }
+}
 
 function articleList(){
-
-    const Article = ReactCreateClass({
-       render: function(){
-           return (
-               <article>
-                   <h2>{this.props.article.name}</h2>
-                   <h3>{this.props.article.description}</h3>
-                   <div>
-                       <p>{this.props.article.author}</p>
-                       <p>{this.props.article.date}</p>
-                   </div>
-               </article>
-           )
-       }
-    });
-
-    const ArticlesList = ReactCreateClass({
-       render: function(){
-           return (
-               <div>
-                   {this.props.articles.map(function(article, key){
-                       return <Article key={key} article={article}/>
-                   })}
-               </div>
-           )
-       }
-    });
 
     function render(articles){
         ReactDom.render(
