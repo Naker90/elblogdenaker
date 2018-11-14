537a6e78f7b1b0a35bc553cfd74e6ac966639744
Naker90 <Antoniojesussg96@gmail.com>
2018-11-14 20:10:04 +0100

    add normalize.css to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("./Content/styles/main-style.css");
 require("./Content/styles/responsive-style.css");
 require("./Content/styles/articles.css");
+require("./Content/styles/normalize.css");
 const routerFactory = require("./Router/routerFactory");
 
 function start(){
