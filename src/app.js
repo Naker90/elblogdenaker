1569a902ed0c78b58d9bed88e69b1b165b0b39b2
Naker90 <Antoniojesussg96@gmail.com>
2018-11-16 22:48:04 +0100

    add new style for screen with min.width

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require("./Content/styles/main-style.css");
 require("./Content/styles/responsive-style.css");
+require("./Content/styles/fullscreen-style.css");
 require("./Content/styles/articles.css");
 require("./Content/styles/normalize.css");
 const routerFactory = require("./Router/routerFactory");
