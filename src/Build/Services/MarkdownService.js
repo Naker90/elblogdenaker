09bca40bfb6f875fd4cd71e49b0f8803b572d892
Naker90 <Antoniojesussg96@gmail.com>
2018-11-25 19:04:55 +0100

    delete console.log)

--- src/Build/Services/MarkdownService.js
+++ src/Build/Services/MarkdownService.js
@@ -3,7 +3,6 @@ function MarkdownService({showdownWrapper, fileSystemWrapper}){
     function convertToHtmlFromMarkdownFile({markdownFilePath}){
         fileSystemWrapper.read({filePath: markdownFilePath})
             .then((markdown) => {
-                console.log(markdown);
                 return showdownWrapper.convertToHtml({markdown: markdown})
             });
     }
