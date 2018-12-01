de7ed6576ab23f35937574c05caf83f060daccda
Naker90 <Antoniojesussg96@gmail.com>
2018-12-01 23:08:05 +0100

    add webpack to build script

--- build.js
+++ build.js
@@ -1,5 +1,12 @@
 const path = require("path");
+const webpack = require("webpack");
+const webpackConfig = require("./webpack.config");
 const BuilderFactory = require("./src/Build/Factory");
+
+webpack(webpackConfig, (error) => {
+    console.log(error);
+});
+
 let builder = BuilderFactory.createBuilder({
     articlesDistPath: path.join(__dirname, "dist/articles")
 });
