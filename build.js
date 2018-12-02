d84c8d00c25b0ffdbd23990df213de18f3eeebc5
Naker90 <Antoniojesussg96@gmail.com>
2018-12-02 17:32:50 +0100

    complete build script

--- build.js
+++ build.js
@@ -1,13 +1,42 @@
+const fs = require("fs");
 const path = require("path");
 const webpack = require("webpack");
 const webpackConfig = require("./webpack.config");
 const BuilderFactory = require("./src/Build/Factory");
 
+console.log("[+] Run Webpack");
 webpack(webpackConfig, (error) => {
-    console.log(error);
+    if(error){
+        console.log(error);
+    }else{
+        console.log("[+] Webpack finished successfully")
+    }
+    transformArticles();
 });
 
-let builder = BuilderFactory.createBuilder({
-    articlesDistPath: path.join(__dirname, "dist/articles")
-});
-builder.build();
+function transformArticles(){
+    console.log("[+] Transforms articles");
+
+    //Por el momento dejo aqui la creacion de la carpeta 'dist/articles'
+    let articlesDistPath = path.join(__dirname, "dist/articles");
+    createPathIfNotExist({path: articlesDistPath});
+
+    transformArticles({articlesDistPath: articlesDistPath});
+    console.log("[+] Build finished successfully");
+
+    function createPathIfNotExist({path}){
+        if(!fs.existsSync(articlesDistPath)){
+            fs.mkdirSync(articlesDistPath);
+        }
+    }
+
+    function transformArticles({articlesDistPath}){
+        let builder = BuilderFactory.createBuilder({
+            articlesDistPath: articlesDistPath
+        });
+        builder.build();
+    }
+}
+
+
+
