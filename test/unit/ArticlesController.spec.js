58f05e87eb160c13f01ff3c91975eb2de51be68d
Naker90 <Antoniojesussg96@gmail.com>
2018-12-16 18:52:32 +0100

    green renders not found

--- test/unit/ArticlesController.spec.js
+++ test/unit/ArticlesController.spec.js
@@ -5,29 +5,40 @@ const JestUtils = require("../utils/JestUtils");
 
 describe("not found controller tests", () => {
 
-    let controller, view, fileReaderService;
+    let controller, view, fileReaderService, context;
 
     beforeEach(() => {
+        context = {
+            params: {
+                year: "2018",
+                month: "05",
+                day: "10",
+                articleName: "anyArticleName"
+            }
+        };
         view = JestUtils.mockAllMethods({obj: ArticleView({})});
         fileReaderService = JestUtils.mockAllMethods({obj: FileReaderService({})});
         controller = ArticleController({view: view, fileReaderService: fileReaderService});
     });
 
-    it("renders article", () => {
-        let context = {
-          params: {
-              year: "2018",
-              month: "05",
-              day: "10",
-              articleName: "anyArticleName"
-          }
-        };
+    it("renders not found", () => {
         fileReaderService.read
             .mockImplementation(({filePath, successCallback, errorCallback}) => {
                 expect(filePath).toEqual("./articles/2018/05/10/anyArticleName.html");
-                return successCallback("any html");
+                return errorCallback();
             });
 
+        controller.execute(context);
+
+        expect(view.render).toHaveBeenCalledWith({content: "Articulo no encontrado."});
+    });
+
+    it("renders article", () => {
+        fileReaderService.read
+            .mockImplementation(({filePath, successCallback}) => {
+                expect(filePath).toEqual(`./articles/${context.params.year}/${context.params.month}/${context.params.day}/${context.params.articleName}.html`);
+                return successCallback("any html");
+            });
 
         controller.execute(context);
 
