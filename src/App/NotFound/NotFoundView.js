059e1076809978de109d2360cba9eacf9a537f84
Naker90 <Antoniojesussg96@gmail.com>
2019-02-23 20:46:38 +0100

    Added container to not found pagei

--- src/App/NotFound/NotFoundView.js
+++ src/App/NotFound/NotFoundView.js
@@ -1,19 +1,21 @@
 function HomeView({renderService}){
 
     const HangmanMarkup = `
-        <h2 class="text-big">404 - Página no encontrada</h2>
-        <input id="start" class="btn btn-big" type="button" value="¡Jugar al ahorcado!">
-        <div id="gameContainer" style="display: none;">
-            <h2 class="text-big">¡Adivina el titulo de las peliculas!</h2>
-            <canvas id="canvas"></canvas>
-            <br>
-            <h2 id="lives">Tienes 10 vidas.</h2>
-            <h3 id="hint-text"></h3>
-            <div id="movie-title"></div>
-            <div id="alphabet"></div>
-            <div>
-                <input id="hint" class="btn" type="button" value="¡Dame una pista!">
-                <input id="play" class="btn" type="button" value="Jugar">
+        <div id="notFoundContainer">
+            <h2 class="text-big">404 - Page not found.</h2>
+            <input id="start" class="btn btn-big" type="button" value="¡Play to Hangman!">
+            <div id="gameContainer" style="display: none;">
+                <h2 class="text-big">Guess the title of the movies!</h2>
+                <canvas id="canvas"></canvas>
+                <br>
+                <h2 id="lives"></h2>
+                <h3 id="hint-text"></h3>
+                <div id="movie-title"></div>
+                <div id="alphabet"></div>
+                <div>
+                    <input id="hint" class="btn" type="button" value="Give me a hint!">
+                    <input id="play" class="btn" type="button" value="Play again!">
+                </div>
             </div>
         </div>`;
 
