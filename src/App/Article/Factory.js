38cc79d87c57797503d4984255e7c0bc2c298416
Naker90 <Antoniojesussg96@gmail.com>
2019-02-23 21:18:18 +0100

    complete factory

--- src/App/Article/Factory.js
+++ src/App/Article/Factory.js
@@ -2,8 +2,12 @@ import RenderService from "../Services/RenderService";
 import ArticleController from "./ArticleController";
 import ArticleView from "./ArticleView";
 import FileReaderService from "../Services/FileReaderService";
+import * as NotFoundFactory from "../NotFound/Factory";
 
 export const createArticleController = () => {
     let view = ArticleView({renderService: RenderService()});
-    return ArticleController({view: view, fileReaderService: FileReaderService()})
+    return ArticleController({
+        notFoundController: NotFoundFactory.createNotFoundController(),
+        view: view,
+        fileReaderService: FileReaderService()})
 };
