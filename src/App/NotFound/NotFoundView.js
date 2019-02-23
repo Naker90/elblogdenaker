a8233f55d167b7238f51346152838f66746c4321
Naker90 <Antoniojesussg96@gmail.com>
2019-02-23 20:34:32 +0100

    Added text

--- src/App/NotFound/NotFoundView.js
+++ src/App/NotFound/NotFoundView.js
@@ -1,10 +1,10 @@
 function HomeView({renderService}){
 
     const HangmanMarkup = `
-        <h2>404 - Página no encontrada</h2>
-        <h2>Si te aburres puedes jugar al ahorcado ;)</h2>
-        <input id="start" class="btn" type="button" value="¡Jugar!">
+        <h2 class="text-big">404 - Página no encontrada</h2>
+        <input id="start" class="btn btn-big" type="button" value="¡Jugar al ahorcado!">
         <div id="gameContainer" class="container" style="display: none;">
+            <h2 class="text-big">¡Adivina el titulo de las peliculas!</h2>
             <canvas id="canvas"></canvas>
             <br>
             <h2 id="lives">Tienes 10 vidas.</h2>
