bf858a3a972ab0857b4582b8f3ce21e8fda44339
Naker90 <Antoniojesussg96@gmail.com>
2019-03-14 00:26:13 +0100

    added new article

--- src/Articles/articles.js
+++ src/Articles/articles.js
@@ -1,6 +1,15 @@
 const path = require("path");
 const articles = [
     {
+        name: "El esquema de datos MySQL aplicado a las inyecciones SQL.",
+        description: "El esquma de datos de MySQL y como podemos sacar partido de el para conocer posibles bases de datos, tablas y campos que existan dentro del gestor de base de datos.",
+        date: "03/09/2019",
+        htmlFileName: "esquema-bd-sql-injection.html",
+        author: "Antonio Sánchez",
+        route: "2018/10/05/esquema-bd-sql-injection",
+        markdownFilePath: path.join(__dirname, "esquema-bd-sql-injection.html.md")
+    },
+    {
         name: "Patrón Modelo-Vista-Presentador con vista pasiva.",
         description: "Una derivación del patrón arquitectónico MVC, y es utilizado mayoritariamente para construir interfaces de usuario.",
         date: "05/10/2018",
