fe687b3567e6d9eacebc8498767e2d03e3662dc3
Naker90 <Antoniojesussg96@gmail.com>
2019-03-30 23:37:44 +0100

    added new route to library

--- src/App/Router/RoutesResolver.js
+++ src/App/Router/RoutesResolver.js
@@ -1,16 +1,19 @@
 import * as HomeFactory from "../Home/Factory";
 import * as NotFoundFactory from "../NotFound/Factory";
 import * as ArticleFactory from "../Article/Factory";
+import * as LibraryFactory from "../Library/Factory";
 
 function RoutesResolver({router: router}){
 
     const homeController = HomeFactory.createHomeController();
     const notFoundController = NotFoundFactory.createNotFoundController();
     const articleController = ArticleFactory.createArticleController();
+    const libraryController = LibraryFactory.createLibraryController();
 
     function init(){
         router.registerRoute({route: "/", resolver: homeController.execute});
         router.registerRoute({route: "/:year/:month/:day/:articleName", resolver: articleController.execute});
+        router.registerRoute({route: "/library", resolver: libraryController.execute});
         router.registerRoute({route: "*", resolver: notFoundController.execute});
         router.finish();
     }
