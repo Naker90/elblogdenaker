19aa07907bc6b9b4f0caed48ad55aa5f8c1ba9f1
Naker90 <Antoniojesussg96@gmail.com>
2019-03-30 23:31:52 +0100

    implemented view

--- src/App/Library/LibraryView.js
+++ src/App/Library/LibraryView.js
@@ -1,11 +1,32 @@
 function LibraryView({renderService}){
 
+    const TRELLO_LINK_MARKUP = `<a href="https://trello.com/b/IeYtjCoV">Trello</a></p>`;
+
     function renderLibrary({library}){
-        throw new Error("not implemented");
+        let markup = `
+        <div>
+            <p>Tambien puedes ver la lista de libros en ${TRELLO_LINK_MARKUP}</p>
+        </div>`;
+        library.forEach((lib) => {
+            markup += `
+                <div>
+                    <h2>${lib.bookcase}</h2>
+                    <ul>
+                        ${lib.books.map((book) => `<li>${book}</li>`)}
+                    </ul>    
+                </div>`
+        });
+        renderService.render({content: markup});
     }
 
     function renderEmptyLibrary(){
-        throw new Error("not implemented");
+        let markup = `
+            <div>
+                <h2>Hubo un problema, 
+                si quieres puedes ver directamente la lista de libros en ${TRELLO_LINK_MARKUP}</h2>
+            </div>
+        `;
+        renderService.render({content: markup});
     }
 
     return {
