565a80e30f7d261fc04cc41e4046feceb589d8b4
Naker90 <Antoniojesussg96@gmail.com>
2019-04-07 20:53:10 +0200

    #refactor move files to another folder

--- test/unit/App/NotFound/NotFoundController.spec.js
+++ test/unit/App/NotFound/NotFoundController.spec.js
@@ -1,7 +1,7 @@
-import NotFoundController from "../../../src/App/NotFound/NotFoundController";
-import NotFoundView from "../../../src/App/NotFound/NotFoundView";
-import Game from "../../../src/App/NotFound/Hangman/Game";
-import JestUtils from "../../utils/JestUtils";
+import NotFoundController from "../../../../src/App/NotFound/NotFoundController";
+import NotFoundView from "../../../../src/App/NotFound/NotFoundView";
+import Game from "../../../../src/App/NotFound/Hangman/Game";
+import JestUtils from "../../../utils/JestUtils";
 
 describe("not found controller tests", () => {
 
